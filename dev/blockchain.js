6b1fd83831109996d7bdf9c906d04566736bcd0c
KonyDev <sampleappsteam@kony.com>
2018-06-15 19:52:44 +0530

    Added proofOfWork and genesis block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,6 +1,10 @@
+ const sha256 = require('sha256');
+ 
  function Blockchain() {
      this.chain = [];
      this.pendingTransactions = [];
+
+     this.createNewBlock(100, '0', '0'); //arbitrary values for our genesis block
  }
 
  Blockchain.prototype.createNewBlock = function(nonce,previousBlockHash,hash) {
@@ -30,8 +34,24 @@
      recipient: recipient
    }
    this.pendingTransactions.push(newTransaction);
-   
+
    return this.getLastBlock()['index'] + 1;
  }
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+}
+
+Blockchain.prototype.proofOfWork = function(previousBlockHash,currentBlockData) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+    while(hash.substring(0,4) !== '0000') {
+      nonce++;
+      hash = this.hashBlock(previousBlockHash,currentBlockData,nonce);
+    }
+
+    return nonce;
+}
 
  module.exports = Blockchain;
