516f3beba9ba7ca01c7865af54d1c074e3f780fa
Shradhey Tripathi <shradhey029tripathi@gmail.com>
2018-06-16 22:10:42 +0530

    Cleaned test file and commit author changed

--- dev/test.js
+++ dev/test.js
@@ -2,34 +2,3 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 console.log(bitcoin);
-// bitcoin.createNewBlock(1234, 'KGJH)*YHJK&JKBJ67fhvjt78787878^$*8','wekjfwkdfbkjdKGGJGKJG');
-// bitcoin.createNewTransaction(100,'sendervd78jbsf7djsdb8','receiverf7gjhgjklgfl698');
-
-// bitcoin.createNewBlock(1234, 'KGJfhvjt78787878^$*8','wekjfwKGGJGKJG');
-
-// bitcoin.createNewTransaction(100,'sendervd78jbsf7djsdb8','receiverf7gjhgjklgfl698');
-// bitcoin.createNewTransaction(200,'sendervd78jbsf7djsdb8','receiverf7gjhgjklgfl698');
-// bitcoin.createNewTransaction(2000,'sendervd78jbsf7djsdb8','receiverf7gjhgjklgfl698');
-
-// bitcoin.createNewBlock(14, 'fhfhf^$ggggggggggggggc*8','wekjfwKGGdsfdfdJGKJG');
-// const previousBlockHash = 'JKSDFJWF45YHJKH3YK45H47454';
-// const currentBlockData = [
-//     {
-//         amount:10,
-//         sender:'fgdfwef',
-//         recipient: 'werfewr'
-//     },
-//     {
-//         amount:30,
-//         sender:'fgdfgggggwef',
-//         recipient: 'werfeggggggwr'
-//     },
-//     {
-//         amount:50,
-//         sender:'feeeweegdfwef',
-//         recipient: 'weeeeeerfewr'
-//     }
-
-// ];
-// console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,371972));
-//console.log(bitcoin.chain[2]);
